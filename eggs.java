5c25d885abe52ba1e92edee9e80c2a07e92b1867
Gautam-10137 <112772090+Gautam-10137@users.noreply.github.com>
2022-11-01 20:25:57 +0530

    eggsInGrossAndDozen

--- eggs.java
+++ eggs.java
@@ -1,15 +1,21 @@
 public class eggs
 {
-  public static void main(String args[]){
-
+  public static void main(String args[])
+ {
   int eggs=Integer.parseInt(args[0]);
+  // no. of eggs in gross
   int gross=eggs/144;
+  
   int aboveGross=eggs%144;
+  // no. of eggs in dozen
   int dozen=aboveGross/12;
+
+  // no. of eggs leftOver
   int leftOver=aboveGross%12;
+    
   System.out.print("Your number of eggs is ");
-  System.out.print(gross+"gross,");
-  System.out.print(dozen+"dozen and");
+  System.out.print(gross+" gross, ");
+  System.out.print(dozen+" dozen and ");
   System.out.println(leftOver);
-}
+ }
 }
